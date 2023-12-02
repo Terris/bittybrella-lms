1e1b25bf850b9953b9ce3d981566dbca7b601ca3
Terris Kremer <terris.kremer@gmail.com>
2023-12-02 08:30:23 -0500

    link to ui files

--- src/lib/ui/CopyToClipboardButton.tsx
+++ src/lib/ui/CopyToClipboardButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button, ButtonProps } from "@/lib/ui/Button";
+import { Button, ButtonProps } from "./Button";
 import { ClipboardCopy, ClipboardCheck, ClipboardX } from "lucide-react";
 import { useState } from "react";
 
