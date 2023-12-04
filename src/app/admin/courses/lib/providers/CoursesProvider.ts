64b2c6db44ccf7023e18f78acdb5cabc00aed9d5
Terris Kremer <terris.kremer@gmail.com>
2023-12-03 22:25:25 -0500

    remove unused locals

---
+++ src/app/admin/courses/lib/providers/CoursesProvider.ts
@@ -0,0 +1,5 @@
+// This file provides data to course components
+// It has a loading component that is displayed while data is being fetched
+// It doesn't render any component
+// It takes in children
+// Children can use the data provided by this component
