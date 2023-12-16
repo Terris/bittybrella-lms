f09cee76d4bf4d055978a7e3f30d033c606aa6fa
Terris Kremer <terris.kremer@gmail.com>
2023-12-16 02:16:26 -0500

    home page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="w-full flex flex-row items-center justify-between py-2 px-8 border-b">
         <Breadcrumbs />
       </div>
-      <div className="py-4 px-8 border-b flex flex-row items-center justify-between">
+      <div className="w-full py-4 px-8 border-b flex flex-row items-center justify-between">
         <Text className="text-3xl font-bold">Home</Text>
       </div>
     </>
