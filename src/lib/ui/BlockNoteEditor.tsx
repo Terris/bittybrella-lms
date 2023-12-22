85123d572fcfe40a727bac03f81b580d2b1c1bd6
Terris Kremer <terris.kremer@gmail.com>
2023-12-21 21:50:09 -0500

    blockquote editor themes

--- src/lib/ui/BlockNoteEditor.tsx
+++ src/lib/ui/BlockNoteEditor.tsx
@@ -2,8 +2,26 @@
 
 import { useTheme } from "next-themes";
 import { BlockNoteEditor as BlockNoteEditorPrimitive } from "@blocknote/core";
-import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import {
+  BlockNoteView,
+  Theme,
+  useBlockNote,
+  darkDefaultTheme,
+} from "@blocknote/react";
 import "@blocknote/core/style.css";
+
+const theme = {
+  ...darkDefaultTheme,
+  componentStyles: (theme) => ({
+    // Adds basic styling to the editor.
+    Editor: {
+      backgroundColor: "red",
+      borderRadius: 0,
+      fontFamily: "Times New Roman",
+      padding: 0,
+    },
+  }),
+} satisfies Theme;
 
 export function BlockNoteEditor() {
   const { resolvedTheme } = useTheme();
@@ -12,10 +30,5 @@ export function BlockNoteEditor() {
     defaultStyles: false,
   });
 
-  return (
-    <BlockNoteView
-      editor={editor}
-      theme={resolvedTheme === "dark" ? "dark" : "light"}
-    />
-  );
+  return <BlockNoteView editor={editor} theme={theme} />;
 }
